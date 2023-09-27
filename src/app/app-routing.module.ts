8aeab3b1748e3c3d5817d7ceb06726a90ec61782
Manuel Maslonka <manuelmaslonka@outlook.sk>
2023-09-27 22:20:09 +0200

    add profile page, search, settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import {NgModule}      from '@angular/core';
+import {NgModule}          from '@angular/core';
 import {
   RouterModule,
   Routes
-}                      from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+}                          from '@angular/router';
+import {HomeComponent}     from "./home/home.component";
 import {SettingsComponent} from "./settings/settings.component";
-import {ProfileComponent} from "./profile/profile.component";
-import {SearchComponent} from "./search/search.component";
+import {ProfileComponent}  from "./profile/profile.component";
+import {SearchComponent}   from "./search/search.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: "full"},
+  {path: '', redirectTo: 'auth', pathMatch: "full"},
   {path: 'home', component: HomeComponent},
   {path: 'settings', component: SettingsComponent},
   {path: 'profile', component: ProfileComponent},
