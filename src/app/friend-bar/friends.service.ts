f73a8d74607d8d4f7a29ab44cee20c1e5d964312
Manuel Mašlonka <manuelmaslonka@icloud.com>
2023-10-27 21:26:42 +0200

    "Improve post and comment operations, and fix minor bugs"
    Significant enhancements have been made to posts and comments functionality. Implemented a more accurate search for posts and comments using unique ID — eliminating the chance of retrieving wrong post or comment when a similar one exists. Also, adjusted the condition for displaying "show more" button in the UI to include situations where the current page equals the maximum page count for more user convenience. Further, the friend's user ID is now fetched dynamically, improving the usability of friend-bar. Finally, fixed a potential issue in friend component which

--- src/app/friend-bar/friends.service.ts
+++ src/app/friend-bar/friends.service.ts
@@ -5,51 +5,51 @@ import {AuthService} from "../auth/auth.service";
 import {User} from "../shared/user.model";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class FriendsService implements OnInit{
+export class FriendsService implements OnInit {
 
-    private baseUrl: string = 'http://localhost:8080/api/';
+  private baseUrl: string = 'http://localhost:8080/api/';
 
-    user!: User;
-    friends: User[] = [];
-    friendsChanged: Subject<User[]> = new Subject<User[]>();
-
-
-    constructor(private http: HttpClient,
-                private authService: AuthService) {
-    }
-
-    ngOnInit(): void {
-        this.authService.user.subscribe(
-            (user) => {
-              console.log("Friends Service")
-              if (user) {
-                console.log(user)
-                this.user = user
-              }
-            }
-        )
-    }
+  user!: User;
+  friends: User[] = [];
+  friendsChanged: Subject<User[]> = new Subject<User[]>();
 
 
+  constructor(private http: HttpClient,
+              private authService: AuthService) {
+  }
 
-    getFriendsByHttp(userId: number) {
-        this.http.get<any>(
-            this.baseUrl + "users/friends/2"
-        ).subscribe(resData => {
-            this.friends = resData;
-            this.updatedFriends()
-        })
-    }
+  ngOnInit(): void {
+    this.authService.user.subscribe(
+      (user) => {
+        console.log("Friends Service")
+        if (user) {
+          console.log(user)
+          this.user = user
+        }
+      }
+    )
+  }
 
-    updatedFriends(): void {
-        this.friendsChanged.next(this.friends.slice());
-    }
 
-    getFriends(): User[] {
-        return this.friends
-    }
+  getFriendsByHttp(userId: number) {
+    this.http.get<any>(
+      this.baseUrl + "users/friends/" + userId,
+    ).subscribe(resData => {
+      console.log(resData);
+      this.friends = resData;
+      this.updatedFriends()
+    })
+  }
+
+  updatedFriends(): void {
+    this.friendsChanged.next(this.friends.slice());
+  }
+
+  getFriends(): User[] {
+    return this.friends
+  }
 
 
 }
--- src/app/home/posts/posts.service.ts
+++ src/app/home/posts/posts.service.ts
@@ -1,176 +1,204 @@
 import {Injectable, OnDestroy, OnInit} from "@angular/core";
 import {Post} from "./post/post.model";
 import {Comment} from "./post/comment/comment.model";
-import {BehaviorSubject, map, Subject} from "rxjs";
+import {BehaviorSubject, map, Subject, tap} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {AuthService} from "../../auth/auth.service";
 import {User} from "../../shared/user.model";
 import {PostPagination} from "../../shared/postPagination.model";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PostsService implements OnInit, OnDestroy {
 
-  private baseUrl: string = 'http://localhost:8080/api/';
-  user!: User;
-  postsChanged: Subject<Post[]> = new Subject<Post[]>();
-  postsList: Post[] = []
-  pageMax = new BehaviorSubject<number>(0);
+    private baseUrl: string = 'http://localhost:8080/api/';
+    user!: User;
+    postsChanged: Subject<Post[]> = new Subject<Post[]>();
+    postsList: Post[] = []
+    pageMax = new BehaviorSubject<number>(0);
 
-  constructor(private http: HttpClient,
-              private authService: AuthService) {
-  }
+    constructor(private http: HttpClient,
+                private authService: AuthService) {
+    }
 
-  ngOnInit(): void {
-    this.authService.user.subscribe(
-      user => {
-        if (user != null) {
-          this.user = user;
-        }
-      }
-    )
-  }
-
-  addPost(content: string, isPrivate: string, imagePath: string) {
-    this.authService.user.subscribe(
-      user => {
-        if (user != null) {
-          this.sendPostToBackend(content, isPrivate, imagePath, user.userId);
-        }
-      }
-    )
-    this.getPostByHttp();
-  }
-
-  getPosts() {
-    return this.postsList.slice();
-  }
-
-  addComment(postId: number, comment: Comment) {
-    this.postsList[postId].comments.unshift(comment);
-  }
-
-  updatePosts() {
-    this.postsChanged.next(this.postsList.slice());
-  }
-
-  getPostByHttp() {
-    this.http.get<Post[]>(
-      this.baseUrl + "posts",
-    ).subscribe(resData => {
-      this.postsList = resData;
-      this.updatePosts();
-    })
-  }
-
-  getPostByHttpPagination(page: number = 0, size: number = 5) {
-    let paramsHttp = new HttpParams();
-    paramsHttp.append("page", page);
-    paramsHttp.append("size", size);
-    console.log(paramsHttp)
-    this.http.get<PostPagination>(
-      this.baseUrl + "posts", {
-        params: new HttpParams().set("page", page).set("size", size)
-      }
-    ).subscribe(resData => {
-        console.log(resData)
-      if (page == 0) {
-        this.postsList = resData.post;
-      } else {
-        this.postsList.push(...resData.post)
-      }
-      this.pageMax.next(resData.totalPages);
-      this.updatePosts();
-    })
-  }
-
-  likeUp(postId: number, post: Post) {
-    console.log(postId)
-    console.log(post)
-    this.postsList[this.postsList.indexOf(post)].likeCount++;
-    this.authService.user.subscribe(
-      user => {
-        if (user) {
-          console.log('Starting likeUp ')
-          this.http.post<boolean>(
-            this.baseUrl + "likes/add/" + user.userId + "/" + postId, {}
-          ).subscribe(
-            resData => {
-              console.log(resData + " this is add likes")
+    ngOnInit(): void {
+        this.authService.user.subscribe(
+            user => {
+                if (user != null) {
+                    this.user = user;
+                }
             }
-          )
+        )
+    }
 
-        }
-      }
-    )
-  }
-
-  likeDown(postId: number, post: Post) {
-
-    this.postsList[this.postsList.indexOf(post)].likeCount--;
-
-    this.authService.user.subscribe(
-      user => {
-        if (user) {
-          console.log('Starting likeUp ')
-          this.http.post<boolean>(
-            this.baseUrl + "likes/remove/" + user.userId + "/" + postId, {}
-          ).subscribe(
-            resData => {
-              console.log(resData + " this is add likes")
+    addPost(content: string, isPrivate: string, imagePath: string) {
+        this.authService.user.subscribe(
+            user => {
+                if (user != null) {
+                    this.sendPostToBackend(content, isPrivate, imagePath, user.userId);
+                }
             }
-          )
+        )
+        this.getPostByHttp();
+    }
 
+    getPosts() {
+        return this.postsList.slice();
+    }
+
+    addComment(postId: number, comment: Comment) {
+        let post = this.postsList.find(post => post.postId === postId)
+        if (post != undefined) {
+            this.postsList[this.postsList.indexOf(post)].comments.unshift(comment);
         }
-      }
-    )
-  }
+    }
 
-  getPostById(postId: number) {
-    return this.postsList[postId];
-  }
+    updatePosts() {
+        this.postsChanged.next(this.postsList.slice());
+    }
+
+    getPostByHttp() {
+        this.http.get<Post[]>(
+            this.baseUrl + "posts",
+        ).subscribe(resData => {
+            this.postsList = resData;
+            this.updatePosts();
+        })
+    }
+
+    getPostByHttpPagination(page: number = 0, size: number = 5) {
+        let paramsHttp = new HttpParams();
+        paramsHttp.append("page", page);
+        paramsHttp.append("size", size);
+        console.log(paramsHttp)
+        this.http.get<PostPagination>(
+            this.baseUrl + "posts", {
+                params: new HttpParams().set("page", page).set("size", size)
+            }
+        ).subscribe(resData => {
+            console.log(resData)
+            if (page == 0) {
+                this.postsList = resData.post;
+            } else {
+                this.postsList.push(...resData.post)
+            }
+            this.pageMax.next(resData.totalPages);
+            this.updatePosts();
+        })
+    }
+
+    likeUp(postId: number, post: Post) {
+        this.postsList[this.postsList.indexOf(post)].likeCount++;
+        this.authService.user.subscribe(
+            user => {
+                if (user) {
+                    console.log('Starting likeUp ')
+                    this.http.post<boolean>(
+                        this.baseUrl + "likes/add/" + user.userId + "/" + postId, {}
+                    ).subscribe(
+                        resData => {
+                            console.log(resData + " this is add likes")
+                        }
+                    )
+
+                }
+            }
+        )
+    }
+
+    likeDown(postId: number, post: Post) {
+
+        this.postsList[this.postsList.indexOf(post)].likeCount--;
+
+        this.authService.user.subscribe(
+            user => {
+                if (user) {
+                    console.log('Starting likeUp ')
+                    this.http.post<boolean>(
+                        this.baseUrl + "likes/remove/" + user.userId + "/" + postId, {}
+                    ).subscribe(
+                        resData => {
+                            console.log(resData + " this is add likes")
+                        }
+                    )
+
+                }
+            }
+        )
+    }
+
+    getPostById(postId: number) {
+        return this.postsList.find(post => post.postId === postId)
+    }
 
 
-  commentLikeUp(postId: number, commentId: number) {
-    this.postsList[postId].comments[commentId].likeCount++
-    this.updatePosts();
-  }
+    commentLikeUp(postId: number, commentId: number) {
+        console.log(commentId);
+        console.log(postId);
+    }
 
-  commentLikeDown(postId: number, commentId: number) {
-    this.postsList[postId].comments[commentId].likeCount--
-    this.updatePosts();
-  }
+    commentLikeDown(postId: number, comment: number) {
+        // this.postsList[postId].comments[commentId].likeCount--
+        // this.updatePosts();
+    }
 
-  isLiked(postId: number, post: Post) {
-    let indexOfPost = this.postsList.indexOf(post)
-    return this.authService.user.pipe(map(user => {
-      if (user != null) {
-        for (let i = 0; i < this.postsList[indexOfPost].likes.length; i++) {
-          if (this.postsList[indexOfPost].likes[i].userId === user.userId) {
-            return true
-          }
-        }
-      }
-      return false
-    }))
-  }
+    isLiked(postId: number, post: Post) {
+        let indexOfPost = this.postsList.indexOf(post)
+        return this.authService.user.pipe(map(user => {
+            if (user != null) {
+                for (let i = 0; i < this.postsList[indexOfPost].likes.length; i++) {
+                    if (this.postsList[indexOfPost].likes[i].userId === user.userId) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }))
+    }
 
-  private sendPostToBackend(content: string, isPrivate: string, imagePath: string,
-                            userId: number) {
+    private sendPostToBackend(content: string, isPrivate: string, imagePath: string,
+                              userId: number) {
 
-    this.http.post<boolean>(
-      this.baseUrl + "posts/create/" + userId, {
-        content: content,
-        isPrivate: isPrivate,
-        imagePath: imagePath
-      }
-    ).subscribe(resData => {
-      this.getPostByHttp()
-    })
-  }
+        this.http.post<boolean>(
+            this.baseUrl + "posts/create/" + userId, {
+                content: content,
+                isPrivate: isPrivate,
+                imagePath: imagePath
+            }
+        ).subscribe(resData => {
+            this.getPostByHttp()
+        })
+    }
 
-  ngOnDestroy() {
-  }
+    ngOnDestroy() {
+    }
 
+    sendCommentToBackend(commentContent: string, postId: number, userId: number) {
+        return this.http.post<any>(
+            this.baseUrl + "comments/create/" + userId + "/" + postId, {
+                'content': commentContent
+            }
+        ).pipe(
+            tap(resData => {
+
+                }
+            )
+        )
+    }
+
+    private sendCommentLikeToBackend(comment: Comment) {
+        this.authService.user.subscribe(
+            user => {
+                if (user) {
+                    this.http.post<boolean>(
+                        this.baseUrl + "likes/comments/add/" + user.userId + "/" + comment.commentsId, {}
+                    ).subscribe(
+                        resData => {
+                            console.log(resData + " this is add likes")
+                        }
+                    )
+                }
+            })
+    }
 }
