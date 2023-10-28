c748df9b07b6b64c67e697533d13d2d225f8ffbb
Manuel Mašlonka <manuelmaslonka@icloud.com>
2023-10-28 15:16:21 +0200

    Implement subscription management in CreateCommentComponent
    In `create-comment.component.ts` subscriptions to `AuthService` and `PostsService` were added but not managed, potentially leading to memory leaks. Implementing `OnDestroy` and adding `unsubscribe` calls on respective subscriptions when the component is destroyed to free up system resources.

--- src/app/home/posts/post/create-comment/create-comment.component.ts
+++ src/app/home/posts/post/create-comment/create-comment.component.ts
@@ -1,46 +1,63 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {PostsService} from "../../posts.service";
 import {Comment} from "../comment/comment.model";
 import {User} from "../../../../shared/user.model";
 import {AuthService} from "../../../../auth/auth.service";
+import {Subscription} from "rxjs";
+
 
 @Component({
-    selector: 'app-create-comment',
-    templateUrl: './create-comment.component.html',
-    styleUrls: ['./create-comment.component.sass']
+  selector: 'app-create-comment',
+  templateUrl: './create-comment.component.html',
+  styleUrls: ['./create-comment.component.sass']
 })
-export class CreateCommentComponent implements OnInit {
+export class CreateCommentComponent implements OnInit, OnDestroy {
 
-    @ViewChild('content')
-    commentContent!: ElementRef;
+  @ViewChild('content')
+  commentContent!: ElementRef;
 
-    user!: User;
-    @Input()
-    postId: number = 0;
+  user!: User;
+  @Input()
+  postId: number = 0;
 
-    constructor(private postsService: PostsService,
-                private authService: AuthService) {
+  userSub = new Subscription();
+  postSub = new Subscription();
+
+  constructor(private postsService: PostsService,
+              private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(
+      user => {
+        if (user) {
+          this.user = user;
+        }
+      }
+    )
+  }
+
+  addCommentToPost(commentContent: string) {
+    let post = this.postsService.getPostById(this.postId)
+    if (post != undefined) {
+      let comment = new Comment(0,
+        commentContent,
+        new Date(),
+        this.user.firstName + " " + this.user.lastName,
+        0,
+        [],
+        this.postId,
+        this.user.userId)
+      this.postsService.addComment(this.postId, comment);
+      this.commentContent.nativeElement.value = '';
     }
+    this.postSub = this.postsService.sendCommentToBackend(commentContent, this.postId, this.user.userId).subscribe();
+  }
 
-    ngOnInit(): void {
-        this.authService.user.subscribe(
-          user => {
-            if (user) {
-              this.user = user;
-            }
-          }
-        )
-    }
-
-    addCommentToPost(commentContent: string) {
-      let post = this.postsService.getPostById(this.postId)
-      if (post != undefined) {
-         let comment = new Comment(0, commentContent , new Date(), this.user.firstName + " " + this.user.lastName, 0, [], this.postId, this.user.userId)
-         this.postsService.addComment(this.postId, comment);
-         this.commentContent.nativeElement.value = '';
-       }
-      this.postsService.sendCommentToBackend(commentContent, this.postId, this.user.userId).subscribe();
-    }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+    this.postSub.unsubscribe();
+  }
 
 
 }
