78cab77deb62ca1fcf5c8cbdfbe644c0c78f5f04
Manuel Mašlonka <manuelmaslonka@icloud.com>
2023-10-28 15:17:19 +0200

    Update UI and functionality of search component
    The search module of the application was enhanced and improved. A UI overhaul was given to search.component.sass and search.component.html, adding styling and interactive elements such as entry fields, buttons, and form validation. The functionality of the search component was also enhanced in search.component.ts to include form submission handling, data fetching from the back-end, and state management. This greatly improves the User Experience (UX) while performing a search in the application.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {SearchService} from "./search.service";
+import {User} from "../shared/user.model";
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.sass']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, OnDestroy {
+
+  searchForm = this.fb.group({
+    search: ['', [Validators.required, Validators.minLength(3)]]
+  })
+  searchedUsers: User[] = [];
+  isSearched: boolean = false;
+  searchSub: Subscription = new Subscription();
+
+  constructor(private fb: FormBuilder,
+              private searchService: SearchService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  get search() {
+    return this.searchForm.get('search')
+  }
+
+  onSubmit(searchContent: string) {
+    this.searchSub = this.searchService.search(searchContent).subscribe(
+      searchedUsers => {
+        this.searchedUsers = searchedUsers;
+        this.isSearched = true;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.searchSub.unsubscribe();
+  }
+
 
 }
